5167b7ce391623bdbadcc3bd4372208336e66123
Gustavo Carvalho <gustavocrvl42@gmail.com>
2024-08-16 10:44:01 -0300

    chore: add tailwind

--- src/components/sections/start/start.tsx
+++ src/components/sections/start/start.tsx
@@ -1,3 +1,14 @@
 export const Start = () => {
-  return <h1>Oi, eu sou o Gustavo</h1>
+  return (
+    <section>
+      <h1 className="text-3xl font-bold font-mono">Oi, eu sou o Gustavo!</h1>
+      <p>
+        Sou Engenheiro de Software e tenho formação em Sistemas de Informação pela Universidade
+        Federal do Sul e Sudeste do Pará (UNIFESSPA). Possuo mais de 3 anos de experiência na área
+        de desenvolvimento web e já trabalhei com várias linguagens e ferramentas tanto em Front-end
+        quanto em Back-end.
+      </p>
+      <button onClick={() => {}}>meu curriculo</button>
+    </section>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Overpass', 'sans-serif'],
+      serif: ['serif'],
+      mono: ['Overpass Mono', 'mono'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
